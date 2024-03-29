f50c63b56d6fe0ac0b74ed2a17e80afacc9a608b
Dmitriy <dmitri1.z@yandex.ru>
2024-03-29 18:59:40 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,72 @@
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import javax.management.Query;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Configuration configuration = new Configuration().configure();
+        try(SessionFactory sessionFactory = configuration.buildSessionFactory()){
+            InsertNewStudents(sessionFactory);//persist
+            FindById(sessionFactory,2);//Find(by id)
+            UpdateAge(sessionFactory);//merge
+            FindWithQL(sessionFactory);//запрос студентов старше 20-ти
+            RemoveStudent(sessionFactory, 4);//remove
+            FindById(sessionFactory,4);
+
+        }
+    }
+
+    private static void FindWithQL(SessionFactory sessionFactory) {
+        try(Session session = sessionFactory.openSession()){
+            session.createQuery("from Student where age > 20", Student.class).getResultList().forEach(System.out::println);
+        }
+    }
+
+    private static void RemoveStudent(SessionFactory sessionFactory,int id) {
+        try(Session session = sessionFactory.openSession()){
+            Transaction tx = session.beginTransaction();
+            Student student = session.find(Student.class, id);
+            session.remove(student);
+            tx.commit();
+        }
+    }
+
+    private static void UpdateAge(SessionFactory sessionFactory) {
+        try(Session session = sessionFactory.openSession()){
+            Transaction tx = session.beginTransaction();
+            Student student = session.find(Student.class, 2);
+            student.setAge(18);
+            session.merge(student);
+            tx.commit();
+            System.out.println(student);
+        }
+    }
+
+    private static void FindById(SessionFactory sessionFactory, int id) {
+        try(Session session = sessionFactory.openSession()){
+            Student student = session.find(Student.class, id);
+            System.out.println(student);
+        }
+    }
+
+    private static void InsertNewStudents(SessionFactory sessionFactory) {
+        Student student = new Student(1, "Itachi", "Uchiha", 15);
+        Student student1 = new Student(2, "Shisui", "Uchiha", 17);
+        Student student2 = new Student(3, "Kakashi", "Hatake", 17);
+        Student student3 = new Student(4, "Zoro", "Roronoa", 21);
+        try(Session session = sessionFactory.openSession()){
+            Transaction tx = session.beginTransaction();
+            session.persist(student);
+            session.persist(student1);
+            session.persist(student2);
+            session.persist(student3);
+            tx.commit();
+        }
+    }
+}
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,73 @@
+package org.example;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "second_name")
+    private String secondName;
+    @Column(name = "age")
+    private Integer age;
+
+    public Student(){
+
+    }
+
+    public Student(Integer id, String name, String secondName, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.secondName = secondName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
